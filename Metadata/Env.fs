5d6829e6045094d056fabe43527194e5556ba3a3
rheophile10 <icar@rheophile.ca>
2024-10-11 23:12:34 -0600

    update env

--- Metadata/Env.fs
+++ Metadata/Env.fs
@@ -2,9 +2,24 @@ namespace Metadata
 
 module Env =
     
-    open DotNetEnv
+    open System
+    open System.IO
+
+    let readEnvFile filePath =
+        if File.Exists filePath then
+            File.ReadAllLines(filePath)
+            |> Array.choose (fun line -> 
+                let parts = line.Split('=')
+                if parts.Length = 2 then Some(parts.[0].Trim(), parts.[1].Trim()) else None)
+            |> Map.ofArray
+        else
+            Map.empty
 
     let getConnectionString (connectionStringEnvKey:string): string =
-        DotNetEnv.Env.Load() |> ignore
-        let connString = DotNetEnv.Env.GetString(connectionStringEnvKey)
-        connString
+        let envFilePath = ".env"
+        envFilePath
+        |> readEnvFile
+        |> Map.tryFind connectionStringEnvKey
+        |> function
+            | Some connString -> connString
+            | None -> failwithf "Connection string with key %s not found." connectionStringEnvKey
